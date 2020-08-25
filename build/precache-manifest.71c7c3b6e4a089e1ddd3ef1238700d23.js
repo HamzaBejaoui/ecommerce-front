010c67ab0e017c6946d25f80c5643d9241fd652a
Hamza Bejaoui <hamza.bejaoui@wevioo.com>
2020-08-25 11:25:03 +0100

    adding build folder

---
+++ build/precache-manifest.71c7c3b6e4a089e1ddd3ef1238700d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b07b924bbabc34990a2b3d74252f964",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcd77e75b9d654d57f29",
+    "url": "/static/css/main.8fc1c253.chunk.css"
+  },
+  {
+    "revision": "49af5fd94b4255567797",
+    "url": "/static/js/2.b6abec52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b6abec52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd77e75b9d654d57f29",
+    "url": "/static/js/main.ff773073.chunk.js"
+  },
+  {
+    "revision": "8b70eb2e35dee98fb26d",
+    "url": "/static/js/runtime-main.d33b66ca.js"
+  }
+]);
